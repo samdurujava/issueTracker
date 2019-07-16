d0db00996bff72bbef58389fc4cc2b1c9988f534
GBTC441007ur <samduru1@gmail.com>
2019-07-16 11:52:43 -0400

    Added new features

--- src/main/java/com/sam/springsecurity/SecurityConfiguration.java
+++ src/main/java/com/sam/springsecurity/SecurityConfiguration.java
@@ -7,6 +7,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.apache.commons.io.filefilter.FileFilterUtils.and;
 
 
 @Configuration
@@ -22,16 +25,28 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception{
         http
                 .authorizeRequests()
+                .antMatchers("/")
+                .access("hasAnyAuthority('USER','ADMIN')")
+                .antMatchers("/admin").access("hasAuthority('ADMIN')")
                 .anyRequest().authenticated()
                 .and()
-                .formLogin().loginPage("/login").permitAll();
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .logout()
+                .logoutRequestMatcher(
+                        new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login").permitAll();
+
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.inMemoryAuthentication().withUser("user")
-                .password(passwordEncoder().encode("password"))
-        .authorities("USER");
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception
+    {
+        auth.inMemoryAuthentication()
+                .withUser("dave").password(passwordEncoder().encode("begreat")).authorities("ADMIN")
+
+                .and()
+                .withUser("user").password(passwordEncoder().encode("password")).authorities("USER");
 
     }
 }
